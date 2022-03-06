c305b11442c16f5c4ec2a830100abd26a6010648
William Slabbaert <william.slabbaert@student.hogent.be>
2022-03-06 18:07:33 +0100

    Player ID remove

--- src/domain/Player.java
+++ src/domain/Player.java
@@ -1,7 +1,6 @@
 package domain;
 
 public class Player {
-    int playerID;
     String playerName;
     int yearOfBirth;
     int gamesCount;
@@ -10,27 +9,18 @@ public class Player {
     public Player(String playerName, int yearOfBirth) {
         this.setPlayerName(playerName);
         this.setYearOfBirth(yearOfBirth);
-        playerID = 1;
         this.setGamesCount(5);
     }
     public Player(int playerID,String playerName, int yearOfBirth, int gamesCount) {
         this.setPlayerName(playerName);
         this.setYearOfBirth(yearOfBirth);
-        this.playerID = playerID;
         this.setGamesCount(gamesCount);
     }
     @Override
     public String toString()
     {
-        return String.format("%s%d%s%s%s%d%s%d","PlayerID: ",this.playerID," PlayerName: ",this.playerName,"Year of birth: ",this.yearOfBirth,"Games left: ", this.gamesCount);
+        return String.format("%s%d%s%s%s%d%s%d"," PlayerName: ",this.playerName,"Year of birth: ",this.yearOfBirth,"Games left: ", this.gamesCount);
 
-    }
-    public int getPlayerID() {
-        return playerID;
-    }
-
-    public void setPlayerID(int playerID) {
-        this.playerID = playerID;
     }
 
     public String getPlayerName() {
