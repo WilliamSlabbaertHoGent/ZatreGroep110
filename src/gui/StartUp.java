f19c38fc703b61ceee07630368a3a8315cad5add
Tommy Rooryck <tommy_rooryck@hotmail.com>
2022-03-20 13:12:26 +0100

    added first css

--- src/gui/StartUp.java
+++ src/gui/StartUp.java
@@ -4,12 +4,22 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class StartUp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         WelcomeScreen root = new WelcomeScreen();
         Scene scene = new Scene(root, 1100, 800);
+
+        URL url = this.getClass().getResource("/css/style.css");
+        if (url == null) {
+            System.out.println("Resource not found.");
+            System.exit(-1);
+        }
+
+        scene.getStylesheets().add(url.toExternalForm());
 
         stage.setScene(scene);
         stage.setTitle("Welcome to Zatre.");
--- src/gui/WelcomeScreen.java
+++ src/gui/WelcomeScreen.java
@@ -4,12 +4,20 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 
 import java.util.Objects;
 
-public class WelcomeScreen extends Pane {
+public class WelcomeScreen extends VBox {
     public WelcomeScreen() {
         Label lblWelcome = new Label("Welcome to Zatre!");
+        lblWelcome.setLayoutY(10);
+        lblWelcome.setLayoutX(200);
+        lblWelcome.getStyleClass().add("title");
+        lblWelcome.setTextAlignment(TextAlignment.CENTER);
+        this.getChildren().add(lblWelcome);
+
         ImageView ivZatre = new ImageView(
                 new Image(
                         getClass().getResourceAsStream(
@@ -17,13 +25,8 @@ public class WelcomeScreen extends Pane {
                         )
                 )
         );
-
-        lblWelcome.setLayoutX(200);
-        lblWelcome.setLayoutY(10);
-
         ivZatre.setLayoutX(50);
         ivZatre.setLayoutY(50);
-
-        this.getChildren().addAll(lblWelcome, ivZatre);
+        this.getChildren().add(ivZatre);
     }
 }
